0e3c87aabc150e37aec5f829c236027360e9dd1c
赵学礼 <xueli.zhao@lilyenglish.com>
2020-09-11 17:42:17 +0800

    [develop]1、实现扩展应用与宿主应用共通公共工具类

---
+++ CollageProExtensionKit/CollageProExtensionKit.h
@@ -0,0 +1,19 @@
+//
+//  CollageProExtensionKit.h
+//  CollageProExtensionKit
+//
+//  Created by admin on 2020/9/11.
+//  Copyright © 2020 jabrknight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CollageProExtensionKit.
+FOUNDATION_EXPORT double CollageProExtensionKitVersionNumber;
+
+//! Project version string for CollageProExtensionKit.
+FOUNDATION_EXPORT const unsigned char CollageProExtensionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollageProExtensionKit/PublicHeader.h>
+
+
